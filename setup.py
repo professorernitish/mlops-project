c0631360821b46fa18f545979570d6de8e78c22d
Nitish Kumar <professornitish@gmail.com>
2025-03-20 23:15:30 +0530

    project structure ready

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Nitish Kumar",
+    author_email="professorernitish@gmail",
+    packages=find_packages()
+)
